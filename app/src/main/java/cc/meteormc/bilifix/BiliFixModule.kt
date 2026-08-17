ff827e7f575699f3fc00198e9607e38a8e2a460d
Meteor2333 <Meteor23333@outlook.com>
2026-08-17 10:46:32 +0800

    feat: 支持热重载

--- app/src/main/java/cc/meteormc/bilifix/BiliFixModule.kt
+++ app/src/main/java/cc/meteormc/bilifix/BiliFixModule.kt
@@ -5,20 +5,30 @@ import cc.meteormc.xposedkit.XLog
 import cc.meteormc.xposedkit.XposedKit
 import cc.meteormc.xposedkit.XposedModule
 import cc.meteormc.xposedkit.annotation.ModuleRegister
+import cc.meteormc.xposedkit.param.HotReloadingParam
 import cc.meteormc.xposedkit.param.PackageLoadedParam
 import cc.meteormc.xposedkit.support.ModuleContextWrapper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 
 @ModuleRegister(
     targetApi = 102,
-    staticScope = true
+    staticScope = true,
+    autoHotReload = true
 )
-class BiliFixModule : XposedModule {
+object BiliFixModule : XposedModule {
     init {
         XLog.level = if (BuildConfig.DEBUG) Log.VERBOSE else Log.INFO
     }
 
+    val ioScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onPackageLoaded(param: PackageLoadedParam) {
         if (!param.isFirstPackage) return
+        if (param.processName != param.packageName) return
+
         XLog.i("BiliFixModule", "Loading module for package: ${param.packageName}")
         XposedKit.registerAppAttachListener(param.packageName) {
             val resources = it.resources
@@ -36,5 +46,10 @@ class BiliFixModule : XposedModule {
                 param.classLoader
             )
         }
+    }
+
+    override fun onHotReloading(param: HotReloadingParam): Boolean {
+        ioScope.cancel()
+        return super.onHotReloading(param)
     }
 }
